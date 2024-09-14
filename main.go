4d9d24e3f7a9bf57779f2d88a18d3fe1aa1f949d
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-09-14 11:20:59 +0900

    :zap: test get stores

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"runtime"
+	"os"
+
+	"github.com/go-resty/resty/v2"
 )
+
+type Store struct {
+	StoreID   string `json:"storeId"`
+	StoreName string `json:"storeName"`
+}
 
 func main() {
 	LoadEnv()
 
-	accessTokenResult, err := GetAccessToken()
+	accessToken, err := GetAccessToken()
 	if err != nil {
 		log.Fatalf("Failed to get access token: %v", err)
 	}
 
-	fmt.Printf("Go Version: %s\n", runtime.Version())
-	fmt.Printf("access_token: %s\n", accessTokenResult)
+	fmt.Println("Access Token:", accessToken)
+
+	client := resty.New()
+
+	// Get stores
+	resp, err := client.R().
+		SetHeader("Authorization", "Bearer "+accessToken).
+		Get(os.Getenv("SMAREGI_API_HOST") + "/" + os.Getenv("SMAREGI_SANDBOX_CONTRACT_ID") + "/pos/stores")
+
+	if err != nil {
+		log.Fatalf("Failed to get stores: %v", err)
+	}
+
+	fmt.Println("Update Response Body:", string(resp.Body()))
+
+	var stores []Store
+	err = json.Unmarshal(resp.Body(), &stores)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal stores response: %v", err)
+	}
+
+	fmt.Println("-----")
+	fmt.Printf("Store Count: %d\n", len(stores))
+	if len(stores) > 0 {
+		fmt.Printf("Store ID: %s\n", stores[0].StoreID)
+		fmt.Printf("Store Name: %s\n", stores[0].StoreName)
+	}
 }
