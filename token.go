1d58d52ace23c0b79f16494a4971b87b8b0ef35b
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-09-14 18:11:19 +0900

    :zap: update get token

--- token.go
+++ token.go
@@ -5,8 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"sync"
-	"time"
 
 	"github.com/go-resty/resty/v2"
 )
@@ -18,21 +16,10 @@ type AccessTokenResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
-var (
-	token          string
-	tokenExpiresAt time.Time
-	mu             sync.Mutex
-)
-
 // fetchNewAccessToken fetches a new access token from the API.
-func fetchNewAccessToken() (*AccessTokenResponse, error) {
-	clientID := os.Getenv("SMAREGI_CLIENT_ID")
-	clientSecret := os.Getenv("SMAREGI_CLIENT_SECRET")
-	smaregiIDPHost := os.Getenv("SMAREGI_IDP_HOST")
-	smaregiSandboxContractID := os.Getenv("SMAREGI_SANDBOX_CONTRACT_ID")
-
-	url := fmt.Sprintf("%s/app/%s/token", smaregiIDPHost, smaregiSandboxContractID)
-	auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", clientID, clientSecret)))
+func getAccessToken(scopes []string) (string, error) {
+	url := fmt.Sprintf("%s/app/%s/token", os.Getenv("SMAREGI_IDP_HOST"), os.Getenv("SMAREGI_SANDBOX_CONTRACT_ID"))
+	auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", os.Getenv("SMAREGI_CLIENT_ID"), os.Getenv("SMAREGI_CLIENT_SECRET"))))
 
 	client := resty.New()
 
@@ -41,36 +28,19 @@ func fetchNewAccessToken() (*AccessTokenResponse, error) {
 		SetHeader("Content-Type", "application/x-www-form-urlencoded").
 		SetFormData(map[string]string{
 			"grant_type": "client_credentials",
-			"scope":      "pos.stores:read",
+			"scope":      joinScopes(scopes),
 		}).
 		Post(url)
 
 	if err != nil {
-		return nil, fmt.Errorf("error making request: %w", err)
+		return "", fmt.Errorf("error making request: %w", err)
 	}
-
-	fmt.Println("Response Body:", string(resp.Body()))
 
 	var accessTokenResult AccessTokenResponse
 	err = json.Unmarshal(resp.Body(), &accessTokenResult)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing response: %w", err)
+		return "", fmt.Errorf("error parsing response: %w", err)
 	}
 
-	return &accessTokenResult, nil
-}
-
-// GetAccessToken returns a valid access token, either from cache or by fetching a new one.
-func GetAccessToken() (string, error) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	newToken, err := fetchNewAccessToken()
-	if err != nil {
-		return "", err
-	}
-
-	token = newToken.AccessToken
-
-	return token, nil
+	return accessTokenResult.AccessToken, nil
 }
