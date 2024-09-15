091e499cbd6d6bcf7e010399c28f60704c2fbeae
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-09-15 12:54:47 +0900

    :zap: improve output response

--- utils/output.go
+++ utils/output.go
@@ -4,39 +4,53 @@ import (
 	"fmt"
 	"log"
 	"reflect"
-
-	"github.com/Tsubasa-2005/sumaregi-go/types"
 )
 
-func PrintResponse(response types.GetProductsResponse) {
+// 単一の構造体の中身を表示する関数
+func PrintSingleStruct(data interface{}) {
+	v := reflect.ValueOf(data)
+	if v.Kind() != reflect.Struct {
+		log.Fatalf("Error: provided variable is not a struct")
+	}
+
+	elemType := v.Type()
+
+	// 横の区切りを追加
+	fmt.Println("+----------------------+----------------------+")
+	fmt.Println("| Field               | Value                |")
+	fmt.Println("+----------------------+----------------------+")
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldName := elemType.Field(i).Name
+		fieldValue := fmt.Sprintf("%v", v.Field(i).Interface())
+
+		// フィールド名とフィールド値の表示
+		fmt.Printf("| %-20s | %-20s |\n", fieldName, fieldValue)
+	}
+
+	// エントリの区切り
+	fmt.Println("+----------------------+----------------------+\n")
+}
+
+// ジェネリックな表示関数
+func PrintResponse(response interface{}) {
 	v := reflect.ValueOf(response)
-	if v.Kind() != reflect.Slice {
-		log.Fatalf("Error: provided variable is not a slice")
-	}
 
-	if v.Len() == 0 {
-		fmt.Println("No data to display")
-		return
-	}
-
-	for i := 0; i < v.Len(); i++ {
-		elem := v.Index(i)
-		elemType := elem.Type()
-
-		// 横の区切りを追加
-		fmt.Println("+----------------------+----------------------+")
-		fmt.Println("| Field               | Value                |")
-		fmt.Println("+----------------------+----------------------+")
-
-		for j := 0; j < elem.NumField(); j++ {
-			fieldName := elemType.Field(j).Name
-			fieldValue := fmt.Sprintf("%v", elem.Field(j).Interface())
-
-			// フィールド名とフィールド値の表示
-			fmt.Printf("| %-20s | %-20s |\n", fieldName, fieldValue)
+	// スライスかどうかをチェック
+	if v.Kind() == reflect.Slice {
+		if v.Len() == 0 {
+			fmt.Println("No data to display")
+			return
 		}
 
-		// エントリの区切り
-		fmt.Println("+----------------------+----------------------+\n")
+		for i := 0; i < v.Len(); i++ {
+			elem := v.Index(i)
+			PrintSingleStruct(elem.Interface())
+		}
+	} else if v.Kind() == reflect.Struct {
+		// スライスでない場合、単一の構造体を表示
+		PrintSingleStruct(response)
+	} else {
+		log.Fatalf("Error: provided variable is not a slice or struct")
 	}
 }
