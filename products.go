02093f338321f59f3956e63015145d67029e3bb6
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-09-15 12:55:54 +0900

    :zap: use query params

--- products.go
+++ products.go
@@ -15,11 +15,11 @@ const (
 func (c *Client) GetProducts(ctx context.Context, opts types.GetProductsOpts) (*types.GetProductsResponse, error) {
 	var result types.GetProductsResponse
 
-	_, err := query.Values(opts)
+	v, err := query.Values(opts)
 	if err != nil {
 		return nil, err
 	}
-	err = c.call(ctx, APIPathProducts, http.MethodGet, nil, nil, &result)
+	err = c.call(ctx, APIPathProducts, http.MethodGet, v, nil, &result)
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +27,7 @@ func (c *Client) GetProducts(ctx context.Context, opts types.GetProductsOpts) (*
 	return &result, nil
 }
 
-func (c *Client) PostProducts(ctx context.Context, params types.PostProductsOpts) (*types.PostProductsResponse, error) {
+func (c *Client) PostProducts(ctx context.Context, params types.PostProductsParams) (*types.PostProductsResponse, error) {
 	var result types.PostProductsResponse
 
 	err := c.call(ctx, APIPathProducts, http.MethodPost, nil, params, &result)
