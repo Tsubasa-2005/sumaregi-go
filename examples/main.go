eb41e118e7d480b4808c2f9b969e61c60ebad01c
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2024-09-15 12:54:14 +0900

    :zap: test GetTransactions

--- examples/main.go
+++ examples/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/Tsubasa-2005/sumaregi-go"
+	"github.com/Tsubasa-2005/sumaregi-go/domain"
 	"github.com/Tsubasa-2005/sumaregi-go/types"
 	"github.com/Tsubasa-2005/sumaregi-go/utils"
 )
@@ -16,18 +18,25 @@ func main() {
 		log.Fatalf("Failed to load environment variables: %v", err)
 	}
 	config := sumaregi.NewConfig(envVari.SmaregiAPIHost, envVari.SmaregiContractID)
-	scopes := []string{sumaregi.ProductsRead, sumaregi.ProductsWrite}
+	scopes := []string{sumaregi.TransactionsRead}
 	client, err := sumaregi.NewClient(config, scopes, envVari)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
 	ctx := context.Background()
+	now := time.Now()
+	transactionDateTimeFrom := now.AddDate(0, 0, -2)
+	transactionDateTimeTo := now.AddDate(0, 0, -1)
 
-	products, err := client.GetProducts(ctx, types.GetProductsOpts{})
+	transactions, err := client.GetTransactions(ctx, types.GetTransactionsOpts{
+		TransactionDateTimeFrom: domain.FormatToISO8601(transactionDateTimeFrom),
+		TransactionDateTimeTo:   domain.FormatToISO8601(transactionDateTimeTo),
+	})
 	if err != nil {
 		log.Fatalf("Failed to get products: %v", err)
 	}
 
-	utils.PrintResponse(*products)
+	utils.PrintResponse(*transactions)
+
 }
